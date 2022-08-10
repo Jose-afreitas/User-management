12285bab772955c0ad2f1e0e5edf12d583dd7998
Jose-afreitas <josealmeida106@gmail.com>
2022-08-10 16:21:02 -0300

    wip -using the application with the FEATCH API

--- server/public/models/User.js
+++ server/public/models/User.js
@@ -80,7 +80,9 @@ class User {
 
     static getUsersStorage() {
 
-        return HttpRequest.get('/users');
+        // return HttpRequest.get('/users');
+        return Fetch.get('/users');
+
 
     }
 
@@ -105,11 +107,15 @@ class User {
 
             if (this.id) {
 
-                promise = HttpRequest.put(`/users/${this.id}`, this.toJSON());
+                // promise = HttpRequest.put(`/users/${this.id}`, this.toJSON());
+                promise = Fetch.put(`/users/${this.id}`, this.toJSON());
+
 
             } else {
 
-                promise = HttpRequest.post(`/users`, this.toJSON());
+                // promise = HttpRequest.post(`/users`, this.toJSON());
+                promise = Fetch.post(`/users`, this.toJSON());
+
 
             }
             promise.then(data => {
@@ -123,7 +129,9 @@ class User {
 
     remove() {
 
-        return HttpRequest.delete(`/users/${this.id}`);
+        // return HttpRequest.delete(`/users/${this.id}`);
+        return Fetch.delete(`/users/${this.id}`);
+
     }
 
 }
